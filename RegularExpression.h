ff707bd094c7c56cb73836240ca7e9c7e4e7a996
北栀倾暖 <9965415571@qq.com>
2016-11-30 17:09:31 +0800

    正则

---
+++ RegularExpression.h
@@ -0,0 +1,26 @@
+//
+//  RegularExpression.h
+//  Text
+//
+//  Created by 曾明 on 16/3/8.
+//  Copyright © 2016年 曾明. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RegularExpression : NSObject
+
+#pragma 正则匹配手机号
++ (BOOL)checkTelNumber:(NSString *) telNumber;
+#pragma 正则匹配用户密码6-18位数字和字母组合
++ (BOOL)checkPassword:(NSString *) password;
+#pragma 正则匹配用户姓名,20位的中文或英文
++ (BOOL)checkUserName : (NSString *) userName;
+#pragma 正则匹配用户身份证号15或18位
++ (BOOL)checkUserIdCard: (NSString *) idCard;
+#pragma 正则匹员工号,12位的数字
++ (BOOL)checkEmployeeNumber : (NSString *) number;
+#pragma 正则匹配URL
++ (BOOL)checkURL : (NSString *) url;
+
+@end
